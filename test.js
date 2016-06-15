6b4f58187abd5a14882e6c826dad80d5444d3cee
syoels <syoels@gmail.com>
2016-06-15 12:53:04 +0300

    Update test.js

--- test.js
+++ test.js
@@ -6,8 +6,8 @@ function initDealzone(){
 	var $fn_container = 
 		"<div class='fn-dealzone-container' style='display:none;'>" + 
 			"<iframe src='https://cdn.rawgit.com/syoels/General/master/test.html'></iframe>" + 
-		"/div>";
-	$('html').prepend($fn_container);
+		"</div>";
+	$('body').prepend($fn_container);
 }
 
 function getDealzoneElement(){
@@ -25,8 +25,11 @@ function showDealzone(){
 		'float': 'right'
 	});
 	$fn_container.css({
+		'position': 'fixed',
+		'z-index': '999999999',
 		'width': '18%', 
 		'float': 'left', 
+		'margin-left': '-20%;',
 		'max-width': '18%', 
 		'overflow': 'hidden', 
 		'background': '#3f91c6'
@@ -44,3 +47,4 @@ function keepHeight(){
 		$fn_container.height(h);
 	});
 }
+
