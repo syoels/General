2885aaa638e0d55a9714c9f2fa6f19cc63f99427
syoels <syoels@gmail.com>
2016-06-28 16:38:36 +0300

    dz adjust theme

--- DealZone/dealzoneParent.js
+++ DealZone/dealzoneParent.js
@@ -1,5 +1,5 @@
 //TODO: move to normal hosting
-var TAG = "Branch_v1.1";
+var TAG = "Branch_v1.2";
 var TARGET = "http://cdn.rawgit.com";
 var DZ_ORIGIN = "http://cdn.rawgit.com";
 var DEALZONE_HTML = "http://cdn.rawgit.com/syoels/General/" + TAG + "/DealZone/dealzone.html";
